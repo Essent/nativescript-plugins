b969450667d4f2890c42344e20e9c48dad9dd341
Osei Fortune <fortune.osei@yahoo.com>
2022-11-15 13:07:02 -0400

    fix(appdynamics): fix demo init

--- tools/demo/nativescript-appdynamics/index.ts
+++ tools/demo/nativescript-appdynamics/index.ts
@@ -6,7 +6,11 @@ export class DemoSharedNativescriptAppdynamics extends DemoSharedBase {
     console.log('test nativescript-appdynamics!');
 
     const ad = new Appdynamics();
-    ad.init('AD-AAA-BBB', 'https://example.com');
+    ad.init({
+      appKey: 'AD-AAA-BBB',
+      collectorURL: 'https://example.com',
+      screenshotURL: 'https://example.com',
+    });
 
     // https://docs.appdynamics.com/appd/22.x/latest/en/end-user-monitoring/mobile-real-user-monitoring/overview-of-the-controller-ui-for-mobile-rum/mobile-sessions
     // this should be run when the application is started up, or maybe when the app is resumed.

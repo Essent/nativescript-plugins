4503f26f7af3d46e44f3214a2a4b4885451030d8
Nathan Walker <walkerrunpdx@gmail.com>
2022-11-21 15:11:41 -0800

    chore: cleanup demos

--- apps/demo-angular/src/plugin-demos/nativescript-medallia.component.ts
+++ apps/demo-angular/src/plugin-demos/nativescript-medallia.component.ts
@@ -1,19 +1,31 @@
 import { Component, NgZone } from '@angular/core';
 import { DemoSharedNativescriptMedallia } from '@demo/shared';
-import { } from '@essent/nativescript-medallia';
+import {} from '@essent/nativescript-medallia';
+import { Page, TextField } from '@nativescript/core';
 
 @Component({
-	selector: 'demo-nativescript-medallia',
-	templateUrl: 'nativescript-medallia.component.html',
+  selector: 'demo-nativescript-medallia',
+  templateUrl: 'nativescript-medallia.component.html',
 })
 export class NativescriptMedalliaComponent {
-  
   demoShared: DemoSharedNativescriptMedallia;
-  
-	constructor(private _ngZone: NgZone) {}
+
+  constructor(public page: Page, private _ngZone: NgZone) {}
 
   ngOnInit() {
-    this.demoShared = new DemoSharedNativescriptMedallia();
+    this.demoShared = new DemoSharedNativescriptMedallia(this.page);
   }
 
+  showSurveyNow() {
+    const inputSurveyId = <TextField>this.page.getViewById('inputSurveyId');
+    this.demoShared.showSurvey(inputSurveyId.text);
+  }
+
+  showSurveyParams() {
+    const params = {};
+    const inputParameterName = <TextField>this.page.getViewById('inputParameterName');
+    const inputParameterValue = <TextField>this.page.getViewById('inputParameterValue');
+    params[inputParameterName.text] = inputParameterValue.text;
+    this.demoShared.setParams(params);
+  }
 }
--- apps/demo-angular/webpack.config.js
+++ apps/demo-angular/webpack.config.js
@@ -2,19 +2,22 @@ const webpack = require('@nativescript/webpack');
 const { resolve } = require('path');
 
 module.exports = (env) => {
-
   webpack.init(env);
   webpack.useConfig('angular');
 
   webpack.chainWebpack((config) => {
+    if (webpack.Utils.platform.getPlatformName() === 'android') {
+      config.entry('application').add('./custom_android/application.android');
+    }
+
     // shared demo code
     config.resolve.alias.set('@demo/shared', resolve(__dirname, '..', '..', 'tools', 'demo'));
   });
 
   // Example if you need to share images across demo apps:
   // webpack.Utils.addCopyRule({
-  //   from: '../../../tools/images', 
-	// 	to: 'images',
+  //   from: '../../../tools/images',
+  // 	to: 'images',
   //   context: webpack.Utils.project.getProjectFilePath('node_modules')
   // });
 

704f6849cd2c1fa31952034a5e1753246541c52f
Osei Fortune <fortune.osei@yahoo.com>
2022-11-21 11:45:23 -0400

    fix(appdynamics): 22.8.0 updates  (#3)

--- packages/nativescript-appdynamics/index.android.ts
+++ packages/nativescript-appdynamics/index.android.ts
@@ -1,13 +1,14 @@
 import { Utils } from '@nativescript/core';
 import { AppdynamicsConfiguration, IAppdynamics, LoggingLevel } from './common';
+import lazy from '@nativescript/core/utils/lazy';
 
-const { AgentConfiguration, Instrumentation } = com.appdynamics.eumagent.runtime;
+const Instrumentation = lazy(() => com.appdynamics.eumagent.runtime.Instrumentation);
 
 // https://docs.appdynamics.com/appd/21.x/21.9/en/end-user-monitoring/mobile-real-user-monitoring/instrument-android-applications
 
 export class Appdynamics implements IAppdynamics {
   public init(config: AppdynamicsConfiguration) {
-    const instrumentationConfig = AgentConfiguration.builder()
+    const instrumentationConfig = com.appdynamics.eumagent.runtime.AgentConfiguration.builder()
       .withAppKey(config.appKey)
       .withContext(Utils.android.getApplicationContext())
       .withCollectorURL(config.collectorURL)
@@ -15,30 +16,30 @@ export class Appdynamics implements IAppdynamics {
       .withLoggingLevel(config.loggingLevel || LoggingLevel.Error)
       .build();
 
-    Instrumentation.start(instrumentationConfig);
+    Instrumentation().start(instrumentationConfig);
   }
 
   public startSessionFrame(name: string) {
-    return Instrumentation.startSessionFrame(name);
+    return Instrumentation().startSessionFrame(name);
   }
 
   public reportMetric(name: string, value: number) {
-    Instrumentation.reportMetric(name, value);
+    Instrumentation().reportMetric(name, value);
   }
 
   public startTimer(name: string) {
-    Instrumentation.startTimer(name);
+    Instrumentation().startTimer(name);
   }
 
   public stopTimer(name: string) {
-    Instrumentation.stopTimer(name);
+    Instrumentation().stopTimer(name);
   }
 
   public setUserData(key: string, value: string) {
-    Instrumentation.setUserData(key, value);
+    Instrumentation().setUserData(key, value);
   }
 
   public requestTracker(url: string) {
-    return Instrumentation.beginHttpRequest(new java.net.URL(url));
+    return Instrumentation().beginHttpRequest(new java.net.URL(url));
   }
 }

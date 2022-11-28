2affffc246e08cba94e2331e3cdc843a7a868209
Nathan Walker <walkerrunpdx@gmail.com>
2022-11-28 09:42:31 -0800

    feat(appdynamics): hooks handling

--- apps/demo-angular/nativescript.config.ts
+++ apps/demo-angular/nativescript.config.ts
@@ -1,14 +1,24 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-	id: 'org.nativescript.plugindemoangular',
-	appResourcesPath: '../../tools/assets/App_Resources',
-	android: {
-		v8Flags: '--expose_gc',
-		markingMode: 'none',
-	},
-	appPath: 'src',
+  id: 'org.nativescript.plugindemoangular',
+  appResourcesPath: '../../tools/assets/App_Resources',
+  android: {
+    v8Flags: '--expose_gc',
+    markingMode: 'none',
+  },
+  appPath: 'src',
   cli: {
-    packageManager: 'npm' 
-  }
+    packageManager: 'npm',
+  },
+  hooks: [
+    {
+      type: 'before-checkForChanges',
+      script: './scripts/nativescript-appdynamics/before-checkForChanges.js',
+    },
+    {
+      type: 'before-watchPatterns',
+      script: './scripts/nativescript-appdynamics/before-watchPatterns.js',
+    },
+  ],
 } as NativeScriptConfig;
